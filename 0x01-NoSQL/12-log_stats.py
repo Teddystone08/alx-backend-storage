002b1e827dbeceb895c8114f59cb2718f5383e2e
Teddystone08 <ernestlrty@hotmail.com>
2024-10-24 23:12:13 +0000

    nginx

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """ Python using pymongo to operate mangos"""
+
 from pymongo import MongoClient
 
 if __name__ == "__main__":
     """ Provides some stats about Nginx logs stored in MongoDB """
+
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
