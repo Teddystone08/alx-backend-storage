3acbc2409bcddc23ca43f4f96f3da2c4f819759b
Teddystone08 <ernestlrty@hotmail.com>
2024-10-24 17:02:42 +0000

    kwarg

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""python function that insert document"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """insert a new document"""
+    retuen mango_collection.insert(kwargs)
